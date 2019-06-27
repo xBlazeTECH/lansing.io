ee84962a0c06d53e8fb1e8600b50bde8120906da
Lansing Nye-Madden <me@lansing.io>
2019-06-27 17:54:19 -0400

    Corrected callback method.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mysql = require('mysql');
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
+
+var mysql_connected;
+var mysql_error;
 
 var mysql_conn = mysql.createConnection({
     socketPath: '/srv/run/mysqld/mysqld.sock',
@@ -11,16 +14,21 @@ var mysql_conn = mysql.createConnection({
     password: 'Lansing123!'
 });
 
+mysql_conn.connect(function(err) {
+    if (err) {
+        mysql_connected = false;
+        mysql_error = err.code;
+    } else {
+        mysql_connected = true;
+    }
+});
+
 var test_mysql_conn = function() {
-    mysql_conn.connect(function(err) {
-        if (err) {
-            console.log(err.code);
-            return "FAILED";
-        } else {
-            console.log('connected...');
-            return "OK";
-        }
-    });
+    if (mysql_connected = false) {
+        return "Failed: " + mysql_error;
+    } else {
+        return "OK!";
+    }
 }
 
 app.get('/', (req, res) => res.send('Hello World!'));
