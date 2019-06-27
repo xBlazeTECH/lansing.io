5a50f2e0729094e848286d552ffca50da23e1000
Lansing Nye-Madden <me@lansing.io>
2019-06-27 17:58:51 -0400

    Added default webpages, assets and static content.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')))
 const port = process.env.PORT;
 
 var mysql_connected;
